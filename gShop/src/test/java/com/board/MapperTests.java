cc0afab8a7a5900c682db13c7dd874da789423cb
DESKTOP-CS2GPSL\audwls <kimmjk3@gmail.com>
2021-12-21 09:23:52 +0900

    게시판 update테스트 진행중

--- gShop/src/test/java/com/board/MapperTests.java
+++ gShop/src/test/java/com/board/MapperTests.java
@@ -6,6 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 import com.board.domain.BoardDTO;
 import com.board.mapper.BoardMapper;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 @SpringBootTest
 class MapperTests {
@@ -26,6 +28,47 @@ class MapperTests {
 
         int result = boardMapper.insertBoard(params);
         System.out.println("결과는 " + result + "입니다..");
+    }
+
+    @Test
+    public void testOfSelectDetail() {
+        BoardDTO board = boardMapper.selectBoardDetail((long) 1);
+        try {
+            String boardJson = new ObjectMapper().writeValueAsString(board);
+
+            System.out.println("=========================");
+            System.out.println(boardJson);
+            System.out.println("=========================");
+
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void testOfUpdate() {
+        BoardDTO params = new BoardDTO();
+
+        params.setPostNumber(1);
+        params.setPostTitle("1번 게시글 제목을 수정합니다.");
+        params.setPostContents("1번 게시글 내용을 수정합니다.");
+        params.setPostCategory(2);
+        params.setPostScore(4);
+
+        int result = boardMapper.updateBoard(params);
+        if (result == 1) {
+            BoardDTO board = boardMapper.selectBoardDetail((long) 1);
+            try {
+                String boardJson = new ObjectMapper().writeValueAsString(board);
+
+                System.out.println("=========================");
+                System.out.println(boardJson);
+                System.out.println("=========================");
+
+            } catch (JsonProcessingException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
 }
