6242049d17e4f71f02a8fcfe1f4378c179106640
DESKTOP-VGGRFPB\cnghd <cnghdwo0@gmail.com>
2021-12-22 17:30:39 +0900

    컨트롤러수정 조인html db데이터 삽입

--- gShop/src/main/java/com/board/controller/UserController.java
+++ gShop/src/main/java/com/board/controller/UserController.java
@@ -4,7 +4,9 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
+import com.board.domain.UserDTO;
 import com.board.service.UserService;
 
 @Controller // 해당 클래스를 컨트롤러로 동작
@@ -18,6 +20,16 @@ public class UserController {
     public void openUserJoin() {
 
         logger.info("회원가입페이지 진입");
+    }
+
+    // 회원가입
+    @PostMapping(value = "/gshop/join.do")
+    public String joinPOST(UserDTO params) throws Exception {
+        logger.info("join 진입");
+        // 회원가입 서비스 실행
+        userService.insertUser(params);
+        logger.info("join Service 성공");
+        return "gshop/join";
     }
 
     // 로그인 페이지이동
