f85762e061a8f3ce62b02bbb1eb278e84b4ba02e
DESKTOP-VGGRFPB\cnghd <cnghdwo0@gmail.com>
2021-12-22 12:52:33 +0900

    유저컨트롤러 ,유저DTO, join.html수정 login.html생성

--- gShop/src/main/java/com/board/controller/UserController.java
+++ gShop/src/main/java/com/board/controller/UserController.java
@@ -7,14 +7,21 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 import com.board.service.UserService;
 
-@Controller
+@Controller // 해당 클래스를 컨트롤러로 동작
 public class UserController {
     @Autowired
     private UserService userService;
 
+    // 회원가입 매핑
     @GetMapping(value = "/user/join.do")
     public String openUserJoin(Model model) {
         return "user/join";
+    }
+
+    // 로그인 매핑
+    @GetMapping("/user/login.do")
+    public String openLogin(Model model) {
+        return "user/login";
     }
 
 }
