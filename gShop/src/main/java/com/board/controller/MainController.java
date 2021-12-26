67a441c7e7eccc63110842d7970f8ace327767e1
DESKTOP-RSQID9I\audwls <kimmjk3@gmail.com>
2021-12-26 13:12:11 +0900

    nav 적용, 경로정리

--- gShop/src/main/java/com/board/controller/MainController.java
+++ gShop/src/main/java/com/board/controller/MainController.java
@@ -6,13 +6,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class MainController {
 
-    @RequestMapping("/gshop/sellmouse")
-    public void main() {
-
-    }
-    
     @RequestMapping("/gshop/index")
-    public void main1() {
+    public void main() {
 
     }
 }
